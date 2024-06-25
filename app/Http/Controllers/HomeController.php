a520a16a88c62c72fc6ff19e36b65046085f65a7
ahmed lasheen <alasheen784@gmail.com>
2024-06-25 15:16:01 +0300

    home edit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,15 +28,59 @@ class HomeController extends Controller
         $invoices_count=invoices::count();
         $invoices_sum=invoices::sum('total');
 
-        $paidinvoices_count=invoices::where('value_status',1)->count();
         $paidinvoices_sum=invoices::where('value_status',1)->sum('total');
+        $paidinvoices_count=invoices::where('value_status',1)->count();
+        $paidinvoices_rate=($paidinvoices_count/$invoices_count)*100;
 
-        $unpaidinvoices_count=invoices::where('value_status',2)->count();
         $unpaidinvoices_sum=invoices::where('value_status',2)->sum('total');
-
-        $partialinvoices_count=invoices::where('value_status',3)->count();
+        $unpaidinvoices_count=invoices::where('value_status',2)->count();
+        $unpaidinvoices_rate=($unpaidinvoices_count/$invoices_count)*100;
+       
         $partialinvoices_sum=invoices::where('value_status',3)->sum('total');
+        $partialinvoices_count=invoices::where('value_status',3)->count();
+        $partialinvoices_rate=($partialinvoices_count/$invoices_count)*100;
 
-        return view('home',compact('invoices_count','invoices_sum','paidinvoices_count','paidinvoices_sum','unpaidinvoices_count','unpaidinvoices_sum','partialinvoices_count','partialinvoices_sum'));
+        $chartjs = app()->chartjs
+            ->name('barChartTest')
+            ->type('bar')
+            ->size(['width' => 350, 'height' => 200])
+            ->labels(['الفواتير الغير المدفوعة', 'الفواتير المدفوعة','الفواتير المدفوعة جزئيا'])
+            ->datasets([
+                [
+                    "label" => "الفواتير الغير المدفوعة",
+                    'backgroundColor' => ['#ec5858'],
+                    'data' => [$unpaidinvoices_rate]
+                ],
+                [
+                    "label" => "الفواتير المدفوعة",
+                    'backgroundColor' => ['#81b214'],
+                    'data' => [$paidinvoices_rate]
+                ],
+                [
+                    "label" => "الفواتير المدفوعة جزئيا",
+                    'backgroundColor' => ['#ff9642'],
+                    'data' => [$partialinvoices_rate]
+                ],
+
+
+            ])
+            ->options([]);
+
+
+        $chartjs_2 = app()->chartjs
+            ->name('pieChartTest')
+            ->type('pie')
+            ->size(['width' => 340, 'height' => 200])
+            ->labels(['الفواتير الغير المدفوعة', 'الفواتير المدفوعة','الفواتير المدفوعة جزئيا'])
+            ->datasets([
+                [
+                    'backgroundColor' => ['#ec5858', '#81b214','#ff9642'],
+                    'data' => [$unpaidinvoices_rate,$paidinvoices_rate,$partialinvoices_rate] 
+                ]
+            ])
+            ->options([]);
+
+
+        return view('home',compact('chartjs','chartjs_2','invoices_count','invoices_sum','paidinvoices_count','paidinvoices_sum','unpaidinvoices_count','unpaidinvoices_sum','partialinvoices_count','partialinvoices_sum'));
     }
 }
